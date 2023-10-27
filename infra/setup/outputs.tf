508c887cd16b0a78005d9187cc3c61d641a2fde3
Mark Winterbottom <londonappdev@duck.com>
2023-10-27 14:35:18 +0100

    added ecr repos

--- infra/setup/outputs.tf
+++ infra/setup/outputs.tf
@@ -8,3 +8,13 @@ output "cd_user_access_key_secret" {
   value       = aws_iam_access_key.cd.secret
   sensitive   = true
 }
+
+output "ecr_repo_app" {
+  description = "ECR repository URL for app image"
+  value       = aws_ecr_repository.app.repository_url
+}
+
+output "ecr_repo_proxy" {
+  description = "ECR repository URL for the proxy image"
+  value       = aws_ecr_repository.proxy.repository_url
+}
