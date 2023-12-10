b64605edea14832fd36aaaa0bc94a77a8a6c513d
Mark Winterbottom <londonappdev@duck.com>
2023-12-10 19:14:59 +0000

    Added permissions for VPC (EC2)

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -89,3 +89,62 @@ resource "aws_iam_user_policy_attachment" "ecr" {
   user       = aws_iam_user.cd.name
   policy_arn = aws_iam_policy.ecr.arn
 }
+
+#########################
+# Policy for EC2 access #
+#########################
+
+data "aws_iam_policy_document" "ec2" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:DescribeVpcs",
+      "ec2:CreateTags",
+      "ec2:CreateVpc",
+      "ec2:DeleteVpc",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DeleteSubnet",
+      "ec2:DeleteSecurityGroup",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DetachInternetGateway",
+      "ec2:DescribeInternetGateways",
+      "ec2:DeleteInternetGateway",
+      "ec2:DetachNetworkInterface",
+      "ec2:DescribeVpcEndpoints",
+      "ec2:DescribeRouteTables",
+      "ec2:DeleteRouteTable",
+      "ec2:DeleteVpcEndpoints",
+      "ec2:DisassociateRouteTable",
+      "ec2:DeleteRoute",
+      "ec2:DescribePrefixLists",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeVpcAttribute",
+      "ec2:DescribeNetworkAcls",
+      "ec2:AssociateRouteTable",
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:RevokeSecurityGroupEgress",
+      "ec2:CreateSecurityGroup",
+      "ec2:AuthorizeSecurityGroupEgress",
+      "ec2:CreateVpcEndpoint",
+      "ec2:ModifySubnetAttribute",
+      "ec2:CreateSubnet",
+      "ec2:CreateRoute",
+      "ec2:CreateRouteTable",
+      "ec2:CreateInternetGateway",
+      "ec2:AttachInternetGateway",
+      "ec2:ModifyVpcAttribute",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "ec2" {
+  name        = "${aws_iam_user.cd.name}-ec2"
+  description = "Allow user to manage EC2 resources."
+  policy      = data.aws_iam_policy_document.ec2.json
+}
+
+resource "aws_iam_user_policy_attachment" "ec2" {
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.ec2.arn
+}
