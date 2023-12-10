a0a595237e79135e6240e13fb6dea2b1b8e8d4c7
Mark Winterbottom <londonappdev@duck.com>
2023-12-10 20:58:04 +0000

    Fix ecs error

--- infra/deploy/database.tf
+++ infra/deploy/database.tf
@@ -23,10 +23,6 @@ resource "aws_security_group" "rds" {
     protocol  = "tcp"
     from_port = 5432
     to_port   = 5432
-
-    security_groups = [
-      aws_security_group.ecs_service.id
-    ]
   }
 
   tags = {
