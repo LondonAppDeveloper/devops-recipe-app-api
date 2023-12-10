a2657f7ad7ae69d22b9c2a2ac600054ee9fb38df
Mark Winterbottom <londonappdev@duck.com>
2023-12-10 20:01:39 +0000

    Created VPC and Internet Gateway

---
+++ infra/deploy/network.tf
@@ -0,0 +1,20 @@
+##########################
+# Network infrastructure #
+##########################
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.1.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+#########################################################
+# Internet Gateway needed for inbount access to the ALB #
+#########################################################
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.prefix}-main"
+  }
+}
