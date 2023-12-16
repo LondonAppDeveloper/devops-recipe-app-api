1eb843fd8d01e8a7dbe786984155df1c490930ee
Mark Winterbottom <londonappdev@duck.com>
2023-12-16 18:04:25 +0000

    Added ECS task role

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -2,6 +2,23 @@
 # ECS Cluster for running app on Fargate.
 ##
 
+resource "aws_iam_policy" "task_execution_role_policy" {
+  name        = "${local.prefix}-task-exec-role-policy"
+  path        = "/"
+  description = "Allow ECS to retrieve images and add to logs."
+  policy      = file("./templates/ecs/task-execution-role-policy.json")
+}
+
+resource "aws_iam_role" "task_execution_role" {
+  name               = "${local.prefix}-task-execution-role"
+  assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "task_execution_role" {
+  role       = aws_iam_role.task_execution_role.name
+  policy_arn = aws_iam_policy.task_execution_role_policy.arn
+}
+
 resource "aws_ecs_cluster" "main" {
   name = "${local.prefix}-cluster"
 }
