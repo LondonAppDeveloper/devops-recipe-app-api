8c4efdbf56f498f5524bf33199aa6abc18cb8614
Mark Winterbottom <londonappdev@duck.com>
2023-12-18 17:34:20 +0000

    Add load balancer security group

---
+++ infra/deploy/load_balancer.tf
@@ -0,0 +1,30 @@
+#################
+# Load Balancer #
+#################
+
+resource "aws_security_group" "lb" {
+  description = "Configure access for the Application Load Balancer"
+  name        = "${local.prefix}-alb-access"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "tcp"
+    from_port   = 8000
+    to_port     = 8000
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
