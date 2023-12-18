9c40edf023a0c5e23ef947944d91464607b36496
Mark Winterbottom <londonappdev@duck.com>
2023-12-18 10:49:07 +0000

    Added task definition

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -43,3 +43,24 @@ resource "aws_cloudwatch_log_group" "ecs_task_logs" {
 resource "aws_ecs_cluster" "main" {
   name = "${local.prefix}-cluster"
 }
+
+resource "aws_ecs_task_definition" "api" {
+  family                   = "${local.prefix}-api"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.task_execution_role.arn
+  task_role_arn            = aws_iam_role.app_task.arn
+
+  container_definitions = jsonencode([])
+
+  volume {
+    name = "static"
+  }
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+}
