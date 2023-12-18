4f62beb572780d950a81793b1c59bdf32ce6858e
Mark Winterbottom <londonappdev@duck.com>
2023-12-18 11:58:37 +0000

    Added task definition for app container

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -55,6 +55,54 @@ resource "aws_ecs_task_definition" "api" {
 
   container_definitions = jsonencode(
     [
+            {
+        name              = "api"
+        image             = var.ecr_app_image
+        essential         = true
+        memoryReservation = 256
+        user              = "django-user"
+        environment = [
+          {
+            name  = "DJANGO_SECRET_KEY"
+            value = var.django_secret_key
+          },
+          {
+            name  = "DB_HOST"
+            value = aws_db_instance.main.address
+          },
+          {
+            name  = "DB_NAME"
+            value = aws_db_instance.main.db_name
+          },
+          {
+            name  = "DB_USER"
+            value = aws_db_instance.main.username
+          },
+          {
+            name  = "DB_PASS"
+            value = aws_db_instance.main.password
+          },
+          {
+            name  = "ALLOWED_HOSTS"
+            value = "*"
+          },
+        ]
+        mountPoints = [
+          {
+            readOnly      = false
+            containerPath = "/vol/web/static"
+            sourceVolume  = "static"
+          }
+        ],
+        logConfiguration = {
+          logDriver = "awslogs"
+          options = {
+            awslogs-group         = aws_cloudwatch_log_group.ecs_task_logs.name
+            awslogs-region        = data.aws_region.current.name
+            awslogs-stream-prefix = "api"
+          }
+        }
+      },
       {
         name              = "proxy"
         image             = var.ecr_proxy_image
--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -25,3 +25,11 @@ variable "db_password" {
 variable "ecr_proxy_image" {
   description = "Path to the ECR repo with the proxy image"
 }
+
+variable "ecr_app_image" {
+  description = "Path to the ECR repo with the API image"
+}
+
+variable "django_secret_key" {
+  description = "Secret key for Django"
+}
