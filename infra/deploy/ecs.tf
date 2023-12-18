419653fc2816c23370ebde4356fc39cad7a7443e
Mark Winterbottom <londonappdev@duck.com>
2023-12-18 14:28:03 +0000

    Added Security Group

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -149,3 +149,35 @@ resource "aws_ecs_task_definition" "api" {
     cpu_architecture        = "X86_64"
   }
 }
+
+resource "aws_security_group" "ecs_service" {
+  description = "Access rules for the ECS service."
+  name        = "${local.prefix}-ecs-service"
+  vpc_id      = aws_vpc.main.id
+
+  # Outbound access to endpoints
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # RDS connectivity
+  egress {
+    from_port = 5432
+    to_port   = 5432
+    protocol  = "tcp"
+    cidr_blocks = [
+      aws_subnet.private_a.cidr_block,
+      aws_subnet.private_b.cidr_block,
+    ]
+  }
+
+  # HTTP inbound access
+  ingress {
+    from_port = 8000
+    to_port   = 8000
+    protocol  = "tcp"
+  }
+}
