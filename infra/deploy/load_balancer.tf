b4b72c2b089a3cc8407e5c959c4e4c411b4b6b3a
Mark Winterbottom <londonappdev@duck.com>
2023-12-18 21:23:49 +0000

    Added load balancer listener

--- infra/deploy/load_balancer.tf
+++ infra/deploy/load_balancer.tf
@@ -47,3 +47,14 @@ resource "aws_lb_target_group" "api" {
     path = "/api/health-check/"
   }
 }
+
+resource "aws_lb_listener" "api" {
+  load_balancer_arn = aws_lb.api.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.api.arn
+  }
+}
