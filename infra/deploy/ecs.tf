b540ce020b86f6aab3298a0d008547ac09f9a6d7
Mark Winterbottom <londonappdev@duck.com>
2023-12-18 10:42:53 +0000

    Added log group

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -36,6 +36,10 @@ resource "aws_iam_role_policy_attachment" "task_ssm_policy" {
   policy_arn = aws_iam_policy.task_ssm_policy.arn
 }
 
+resource "aws_cloudwatch_log_group" "ecs_task_logs" {
+  name = "${local.prefix}-api"
+}
+
 resource "aws_ecs_cluster" "main" {
   name = "${local.prefix}-cluster"
 }
