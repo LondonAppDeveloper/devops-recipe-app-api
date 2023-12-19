35835434f298d0e49d17d7a018f47ef6b715180e
Mark Winterbottom <londonappdev@duck.com>
2023-12-19 15:16:24 +0000

    Added security group configuration for EFS

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -174,6 +174,17 @@ resource "aws_security_group" "ecs_service" {
     ]
   }
 
+  # NFS Port for EFS volumes
+  egress {
+    from_port = 2049
+    to_port   = 2049
+    protocol  = "tcp"
+    cidr_blocks = [
+      aws_subnet.private_a.cidr_block,
+      aws_subnet.private_b.cidr_block,
+    ]
+  }
+
   # HTTP inbound access
   ingress {
     from_port = 8000
--- infra/deploy/efs.tf
+++ infra/deploy/efs.tf
@@ -8,3 +8,18 @@ resource "aws_efs_file_system" "media" {
     Name = "${local.prefix}-media"
   }
 }
+
+resource "aws_security_group" "efs" {
+  name   = "${local.prefix}-efs"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port = 2049
+    to_port   = 2049
+    protocol  = "tcp"
+
+    security_groups = [
+      aws_security_group.ecs_service.id
+    ]
+  }
+}
