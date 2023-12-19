a212dc92aad38ade855026821456b6dbde9c0379
Mark Winterbottom <londonappdev@duck.com>
2023-12-19 16:55:18 +0000

    Added Route53 zone

---
+++ infra/deploy/dns.tf
@@ -0,0 +1,12 @@
+data "aws_route53_zone" "zone" {
+  name = "${var.dns_zone_name}."
+}
+
+resource "aws_route53_record" "app" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "${lookup(var.subdomain, terraform.workspace)}.${data.aws_route53_zone.zone.name}"
+  type    = "CNAME"
+  ttl     = "300"
+
+  records = [aws_lb.api.dns_name]
+}
