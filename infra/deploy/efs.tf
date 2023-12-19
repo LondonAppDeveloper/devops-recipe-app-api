5526e1239976f4bd1fef2e141ae0e688694ce9e0
Mark Winterbottom <londonappdev@duck.com>
2023-12-19 15:59:13 +0000

    Added EFS access point

--- infra/deploy/efs.tf
+++ infra/deploy/efs.tf
@@ -35,3 +35,15 @@ resource "aws_efs_mount_target" "media_b" {
   subnet_id       = aws_subnet.private_b.id
   security_groups = [aws_security_group.efs.id]
 }
+
+resource "aws_efs_access_point" "media" {
+  file_system_id = aws_efs_file_system.media.id
+  root_directory {
+    path = "/api/media"
+    creation_info {
+      owner_gid   = 101
+      owner_uid   = 101
+      permissions = "755"
+    }
+  }
+}
