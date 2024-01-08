e8ecbce7e3ad72f39e7f37f8e8470f57dd998793
Mark Winterbottom <londonappdev@duck.com>
2024-01-08 17:37:49 +0000

    Added network subnets

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -81,3 +81,26 @@ resource "aws_route" "public_internet_access_b" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
 }
+
+############################################
+# Private Subnets for internal access only #
+############################################
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.10.0/24"
+  availability_zone = "${data.aws_region.current.name}a"
+
+  tags = {
+    Name = "${local.prefix}-private-a"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.11.0/24"
+  availability_zone = "${data.aws_region.current.name}b"
+
+  tags = {
+    Name = "${local.prefix}-private-b"
+  }
+}
