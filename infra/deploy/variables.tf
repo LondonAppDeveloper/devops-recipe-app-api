3482f945109dbc4165ec9587d7de15752f4d3d2b
Mark Winterbottom <londonappdev@duck.com>
2024-01-08 17:55:01 +0000

    added vars

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -12,3 +12,12 @@ variable "contact" {
   description = "Contact email for tagging resources"
   default     = "mark@example.com"
 }
+
+variable "db_username" {
+  description = "Username for the recipe app api database"
+  default     = "recipeapp"
+}
+
+variable "db_password" {
+  description = "Password for the Terraform database"
+}
